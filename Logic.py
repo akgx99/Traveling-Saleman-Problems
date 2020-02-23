ca5f06a0edfeddf94b9460eb0a328492c2aaae47
Thomas Bénas <akgx99@gmail.com>
2020-02-23 16:12:54 +0100

    Construtor, getter, setter Class Logic

--- Logic.py
+++ Logic.py
@@ -1,1 +1,20 @@
 import City as c
+
+class Logic():
+    """ Classe servant à représenter la couche métier. elle contient :
+    - Une liste des villes (City)
+    """
+
+    def __init__(self):
+        self._cities = []
+
+    
+    """ Getter & Setter """
+    def get_cities(self):
+        return self._cities
+
+    def set_cities(self, cities):
+        self._cities = cities
+    
+    """ Property """
+    cities = property(get_cities, set_cities)
