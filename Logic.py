65e6c5508b4a00b884bec8a7a2afacd54b5a8ea9
Thomas Bénas <akgx99@gmail.com>
2020-02-24 10:43:08 +0100

    addCities function

--- Logic.py
+++ Logic.py
@@ -1,4 +1,4 @@
-import City as c
+import Parser as parse
 
 class Logic():
     """ Classe servant à représenter la couche métier. elle contient :
@@ -6,14 +6,27 @@ class Logic():
     """
 
     def __init__(self):
-        """Constructeur de la classe Logic """
+        """ Constructeur de la classe Logic """
         self._cities = []
     
     def addCity(self, city):
-        """Permet d'ajouter une ville à la liste 
+        """ Permet d'ajouter une ville à la liste 
         - city : la ville à ajouter
         """
         self.cities.append(city)
+
+    def addCities(self):
+        """ Permet depuis un fichier (définit par l'utilisateur) d'ajouter un ensemble de ville """
+        FileParser = parse.Parser()
+        FileParser.open()
+        FileParser.extractContents()
+        self.cities.append(FileParser.createCities())
+
+    def displayCities(self):
+        """ Permet d'afficher les informations des villes """
+        for li in self.cities:
+            for city in li:
+                print(city)
 
     """ Getter & Setter """
     def get_cities(self):
