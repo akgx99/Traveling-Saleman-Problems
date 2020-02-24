ef7e84b55fab66642babc37932f8b99b86d094c3
Thomas Bénas <akgx99@gmail.com>
2020-02-24 15:30:55 +0100

    Main test IncreasingTour()

--- Main.py
+++ Main.py
@@ -1,9 +1,15 @@
 import CitiesManager
+import Logic
 
 def main():
-   process = CitiesManager.CitiesManager()
-   process.addCities()
-   process.displayCities()
+   manager = CitiesManager.CitiesManager()
+   manager.addCities()
+
+   algo = Logic.Logic(manager.cities, manager.cities[0])
+   algo.increasingTour()
+
+   manager.displayCities()
+   print("Le coût est de ", algo.costs)
 
 if __name__ == "__main__":
    main()
