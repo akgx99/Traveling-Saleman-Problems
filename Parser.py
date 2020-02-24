ba036824093946ce6c3f6347626f7f4e84175ed2
Thomas Bénas <akgx99@gmail.com>
2020-02-24 10:41:46 +0100

    extratContents function
    extraire les données d'un fichier

--- Parser.py
+++ Parser.py
@@ -1,10 +1,16 @@
+import City
+
 class Parser():
-    """ Classe parseur servant à parcourir un fichier pour en extraire des données :
+    """ Classe parseur servant à parcourir un fichier pour en extraire des données et créer des objets :
     - file : le fichier à parcourir
+    - data : les données triées du fichier
+
+    Les données sont stockées ligne par ligne. Ligne qui contiennent des éléments (ex : la ligne : "35 47.416667 4.366667" contient trois éléments)
     """
 
     def __init__(self):
         self._file = None
+        self._data = []
 
     def open(self):
         """ Permet d'ouvir un fichier """
@@ -13,9 +19,16 @@ class Parser():
             self.file = open(name, 'r')
         except IOError:
             print('Cannot open : ', name)
-        self.file.close()
-
-    
+      
+    def extractContents(self):
+        """ Permet d'extraire sous forme de liste les données """
+        try:
+            with self.file as f:
+                for line in f:
+                    self._data.append(line)
+            self._file.close()
+        except:
+            print("impossible to recover the data")
 
     """ Getter & Setter """
     def get_file(self):
@@ -23,6 +36,13 @@ class Parser():
 
     def set_file(self, file):
         self._file = file
+    
+    def get_data(self):
+        return self._data
+
+    def set_data(self, data):
+        self._data = data
 
     """ Property """
     file = property(get_file, set_file)
+    data = property(get_data, set_data)
