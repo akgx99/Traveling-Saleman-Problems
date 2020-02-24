79d6a743bc61ee19ccc9c66824bdf14fb12d081b
Thomas Bénas <akgx99@gmail.com>
2020-02-24 15:30:32 +0100

    IncreasingTour function

--- Logic.py
+++ Logic.py
@@ -13,6 +13,17 @@ class Logic():
         self._start = start
         self._costs = 0.00
         
+    def increasingTour(self):
+        """ Algo de parcours des villes une à une par odre croissant de leur position """
+        self.start.visited = True
+
+        for vector in self._vectors:
+            vector.visited = True
+            suivant = self._vectors[+1]
+
+            # calcul de distance de coordonnées selon le thèoréme de Pythagore
+            self._costs += sqrt( (suivant.longitude - vector.latitude) ** 2 + (suivant.latitude - vector.longitude) ** 2) 
+
     """ Getter & Setter """
     def get_vectors(self):
         return self._vectors
