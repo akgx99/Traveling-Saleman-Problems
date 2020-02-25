cce480d7a1b2ff17d58da32913d7c7216d8a3891
Thomas Bénas <akgx99@gmail.com>
2020-02-25 09:00:43 +0100

    comment main file

--- Main.py
+++ Main.py
@@ -2,14 +2,16 @@ import CitiesManager
 import Logic
 
 def main():
-   manager = CitiesManager.CitiesManager()
-   manager.addCities()
+   manager = CitiesManager.CitiesManager() # construteur du CitiesManager
+   manager.addCities() # ajouter des villes a partir d"un fichier
 
-   algo = Logic.Logic(manager.cities, manager.cities[0])
-   algo.increasingTour()
+   if(len(manager.cities) > 0): # si plus d'une ville existe
 
-   manager.displayCities()
-   print("Le coût est de "+ str(algo.costs)+" Km")
+      algo = Logic.Logic(manager.cities, manager.cities[0]) # définit les villes comme sommets et un sommet de départ
+      algo.increasingTour() # lance l'algoo de parcours IncreasingTour
+
+      manager.displayCities() # Affiche les villes qui ont été visité ou non
+      print(algo.costs) # affiche le côut
 
 if __name__ == "__main__":
    main()
